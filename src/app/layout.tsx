b9986cb29ad78391faa6584cb4cdbe5db34bc3df
Hilman N <hilmannafiin9@gmail.com>
2026-09-15 16:17:25 +0700

    live_chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
+import TawkTo from "@/components/TawkTo";
 import { Inter } from "next/font/google";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
           {children}
           <Footer />
           <ScrollToTop />
+          <TawkTo />
         </Providers>
       </body>
     </html>
